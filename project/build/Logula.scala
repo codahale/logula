95076fd3deae43aed465d3ace9d124a6f42117b2
Coda Hale <coda.hale@gmail.com>
2010-05-11 09:57:33 -0700

    Figured out RC2 compilation issues.

--- project/build/Logula.scala
+++ project/build/Logula.scala
@@ -17,11 +17,15 @@ class Logula(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poster
     sbt.Process("rsync", "-avz" :: "target/repo/" :: "codahale.com:/home/codahale/repo.codahale.com" :: Nil) ! log
     None
   } describedAs("Publish binary and source JARs to repo.codahale.com") dependsOn(test, publishToLocalRepoAction)
-  
+
   /**
    * Dependencies
    */
   val scalaToolsSnapshots = "scala-tools.org Snapshots" at "http://scala-tools.org/repo-snapshots"
-  val scalaTest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
+  val scalaTest = buildScalaVersion match {
+    case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
+    case unknown => error("no known scalatest impl for %s".format(unknown))
+  }
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
 }
