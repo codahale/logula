bdfd52cf5b6369227afb41b17be15b12ce29ea3e
Coda Hale <coda.hale@gmail.com>
2010-07-09 14:29:41 -0700

    Cross-build against RC6 and RC7.

--- project/build/Logula.scala
+++ project/build/Logula.scala
@@ -20,6 +20,8 @@ class Logula(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poster
     case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
     case "2.8.0.RC3" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test" withSources() intransitive()
     case "2.8.0.RC5" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC5-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC6" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC7" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC7-SNAPSHOT" % "test" withSources() intransitive()
     case unknown => error("no known scalatest impl for %s".format(unknown))
   }
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
