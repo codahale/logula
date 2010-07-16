47bdde0099bd1dda5d4fdd1818138a1e759ab9e8
Coda Hale <coda.hale@gmail.com>
2010-07-15 18:57:33 -0700

    Bump to 1.0.3.

--- project/build/Logula.scala
+++ project/build/Logula.scala
@@ -15,14 +15,6 @@ class Logula(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poster
    * Dependencies
    */
   val scalaToolsSnapshots = "scala-tools.org Snapshots" at "http://scala-tools.org/repo-snapshots"
-  val scalaTest = buildScalaVersion match {
-    case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC3" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC5" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC5-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC6" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC7" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC7-SNAPSHOT" % "test" withSources() intransitive()
-    case unknown => error("no known scalatest impl for %s".format(unknown))
-  }
+  val scalaTest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test" withSources() intransitive()
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
 }
