54903c42042fa488a5aff963b4c90ce9af49bf56
Coda Hale <coda.hale@gmail.com>
2011-01-06 12:10:07 -0800

    Bump to sbt-idea 0.2.0 and switch to maven-sbt 0.1.0-SNAPSHOT.

--- project/build/Logula.scala
+++ project/build/Logula.scala
@@ -1,4 +1,6 @@
-class Logula(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with IdeaProject with posterous.Publish with rsync.RsyncPublishing {
+import sbt._
+
+class Logula(info: ProjectInfo) extends DefaultProject(info) with IdeaProject with posterous.Publish with maven.MavenDependencies {
   /**
    * Publish the source as well as the class files.
    */
@@ -8,19 +10,20 @@ class Logula(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with IdeaPr
 
   override def compileOptions = super.compileOptions ++
     Seq(Deprecation, ExplainTypes, Unchecked)
+  
+  lazy val publishTo = Resolver.sftp("repo.codahale.com",
+                                     "codahale.com",
+                                     "/home/codahale/repo.codahale.com/")
 
-  /**
-   * Publish via rsync.
-   */
-  def rsyncRepo = "codahale.com:/home/codahale/repo.codahale.com"
+  val codaRepo = "Coda's Repo" at "http://repo.codahale.com"
 
   /**
    * Dependencies
    */
-  val log4j = "log4j" % "log4j" % "1.2.16" withSources() intransitive()
-  val log4jExtras = "log4j" % "apache-log4j-extras" % "1.0" withSources() intransitive()
+  val log4j = "log4j" % "log4j" % "1.2.16"
+  val log4jExtras = "log4j" % "apache-log4j-extras" % "1.0"
 
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test" withSources ()
-  val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test" withSources ()
-  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources ()
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
+  val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test"
+  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test"
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,8 +3,8 @@ class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
   val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
 
   val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
-  val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1.0"
+  val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.2.0"
 
   val codasRepo = "codahale.com" at "http://repo.codahale.com/"
-  val rsync = "com.codahale" % "rsync-sbt" % "0.1.1"
+  val rsync = "com.codahale" % "maven-sbt" % "0.1.0-SNAPSHOT"
 }
